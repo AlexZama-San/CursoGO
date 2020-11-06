f0d29e924f627735cfabb7640a016e83f265e36c
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-11-06 14:50:44 -0600

    Constante

--- Codigo1.go
+++ Codigo1.go
@@ -8,7 +8,8 @@ func main() {
 	var suma int = 8 + 9
 	var resta int = 6 - 4
 	var nombre string = "Alex Rojas"
-	fmt.Println("la suma de los numeros 8 y 9 es igual a",suma)
+	const pi float32=3.1416
+	fmt.Println("la suma de los numeros 8 y 9 es igual a",suma,", Pi es igual a",pi)
 	fmt.Println(resta)
 	fmt.Println(nombre)
 	
