6501e46a7219def3e6b9f82260489d7da2c48a4e
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-11-06 14:46:26 -0600

    first commit

---
+++ Codigo1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var suma int = 8 + 9
+	var resta int = 6 - 4
+	var nombre string = "Alex Rojas"
+	fmt.Println("la suma de los numeros 8 y 9 es igual a",suma)
+	fmt.Println(resta)
+	fmt.Println(nombre)
+	
+}
