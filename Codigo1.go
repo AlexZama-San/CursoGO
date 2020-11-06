a6a697a40ea68b792f740b6296e3896d2534b32c
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-11-06 16:01:11 -0600

    parametros y pruebas

--- Codigo1.go
+++ Codigo1.go
@@ -11,27 +11,52 @@ type persona struct{
 }
 
 func main() {
-	var yo = persona{
+/*	var yo = persona{
 		"Alexis",
 		65,
 		1.67}
 	
-	fmt.Println(yo.nombre)
-	var numeroX int = 10;
-	var numeroY int = 6;
+	fmt.Println(yo.nombre)*/
+	/*var numeroX float32 = 10;
+	var numeroY float32 = 6;
+	fmt.Println("Calculadora 1")
+	Calculadora(numeroX,numeroY)*/
+	fmt.Println(personas(20,21))
 
-	//suma
-	fmt.Println("la suma es:",numeroX+numeroY)
-
-	//resta
-	fmt.Println("la resta es:",numeroX-numeroY)
-
-	//multiplicacion
-	fmt.Println("la multiplicacion es:",numeroX*numeroY)
-
-	//division
-	fmt.Println("la division es:",numeroX/numeroY)
-
-	//residuo
-	fmt.Println("el residuo de la division es:",numeroX%numeroY)
 }
+
+
+	func Calcula(nX float32, nY float32, op string) float32{
+		var resultado float32
+		if (op=="+"){
+			resultado=nX+nY
+		}else if(op=="-"){
+			resultado=nX-nY
+		}else if(op=="*"){
+			resultado=nX*nY
+		}else if(op=="/"){
+			resultado=nX/nY
+	}
+	return resultado
+	}
+	func Calculadora(numeroX float32,numeroY float32){
+			//suma
+		fmt.Println("la suma es:",Calcula(numeroX,numeroY,"+"))
+
+		//resta
+		fmt.Println("la resta es:",Calcula(numeroX,numeroY,"-"))
+
+		//multiplicacion
+		fmt.Println("la multiplicacion es:",Calcula(numeroX,numeroY,"*"))
+
+		//division
+		fmt.Println("la division es:",Calcula(numeroX,numeroY,"/"))
+	}
+
+	func personas(numpersonas int, edad int) (string){
+		totalpersonas:=func() int{
+			return numpersonas*10
+		}
+		var mensaje string=fmt.Sprintf("El numero de personas es %d con edad de %d",totalpersonas(),edad)
+		return mensaje
+	}
