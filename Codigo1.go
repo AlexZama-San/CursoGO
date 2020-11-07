0ad9caf9d2f03a311dee344b27e4f34601491e96
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-11-06 23:24:29 -0600

    Arrays y slices

--- Codigo1.go
+++ Codigo1.go
@@ -4,67 +4,85 @@ import (
 	"fmt"
 )
 
-type persona struct{
+type persona struct {
 	nombre string
-	peso int
+	peso   int
 	altura float32
 }
 
 func main() {
-/*	var yo = persona{
+	/*	var yo = persona{
 		"Alexis",
 		65,
 		1.67}
-	
+
 	fmt.Println(yo.nombre)*/
 	/*var numeroX float32 = 10;
 	var numeroY float32 = 6;
 	fmt.Println("Calculadora 1")
 	Calculadora(numeroX,numeroY)*/
-	fmt.Println(personas(20,21))
+	//fmt.Println(personas(20,21))
 
-	lenguajes("Español","Ingles","Japones")
+	//lenguajes("Español","Ingles","Japones")
+	pruebaSlice()
+	pruebaArray()
 
 }
 
-
-	func Calcula(nX float32, nY float32, op string) float32{
-		var resultado float32
-		if (op=="+"){
-			resultado=nX+nY
-		}else if(op=="-"){
-			resultado=nX-nY
-		}else if(op=="*"){
-			resultado=nX*nY
-		}else if(op=="/"){
-			resultado=nX/nY
+func Calcula(nX float32, nY float32, op string) float32 {
+	var resultado float32
+	if op == "+" {
+		resultado = nX + nY
+	} else if op == "-" {
+		resultado = nX - nY
+	} else if op == "*" {
+		resultado = nX * nY
+	} else if op == "/" {
+		resultado = nX / nY
 	}
 	return resultado
+}
+func Calculadora(numeroX float32, numeroY float32) {
+	//suma
+	fmt.Println("la suma es:", Calcula(numeroX, numeroY, "+"))
+
+	//resta
+	fmt.Println("la resta es:", Calcula(numeroX, numeroY, "-"))
+
+	//multiplicacion
+	fmt.Println("la multiplicacion es:", Calcula(numeroX, numeroY, "*"))
+
+	//division
+	fmt.Println("la division es:", Calcula(numeroX, numeroY, "/"))
+}
+
+func personas(numpersonas int, edad int) string {
+	totalpersonas := func() int {
+		return numpersonas * 10
 	}
-	func Calculadora(numeroX float32,numeroY float32){
-			//suma
-		fmt.Println("la suma es:",Calcula(numeroX,numeroY,"+"))
+	var mensaje string = fmt.Sprintf("El numero de personas es %d con edad de %d", totalpersonas(), edad)
+	return mensaje
+}
 
-		//resta
-		fmt.Println("la resta es:",Calcula(numeroX,numeroY,"-"))
-
-		//multiplicacion
-		fmt.Println("la multiplicacion es:",Calcula(numeroX,numeroY,"*"))
-
-		//division
-		fmt.Println("la division es:",Calcula(numeroX,numeroY,"/"))
+func lenguajes(lenguajes ...string) {
+	for _, lenguaje := range lenguajes {
+		fmt.Println(lenguaje)
+	}
+}
+func pruebaArray(){
+	coches:=[3]string{"Lamborguini","Bugatti","Ferrari"}
+	for _,impcoche:= range coches{
+		fmt.Printf("%s \n",impcoche)
 	}
 
-	func personas(numpersonas int, edad int) (string){
-		totalpersonas:=func() int{
-			return numpersonas*10
-		}
-		var mensaje string=fmt.Sprintf("El numero de personas es %d con edad de %d",totalpersonas(),edad)
-		return mensaje
+}
+func pruebaSlice(){
+	peliculas:=[]string{
+		"Rapido y furioso",
+		"Harry potter",
+		"Ready Player One"}
+	peliculas= append(peliculas,"Como niños")
+	for _, pelicula := range peliculas{
+		fmt.Println(pelicula)
 	}
-
-	func lenguajes(lenguajes ...string){
-		for _,lenguaje := range lenguajes{
-			fmt.Println(lenguaje)
-		}
-	}
+}
